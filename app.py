fda17cb32b8d3646cee3ea8d7814edaeb9513a8f
User <ken990309@gmail.com>
2023-04-09 16:57:04 +0800

    ChiaHong's HW3 Version2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # main.py
- 
 from flask import Flask, render_template
+from CH_DBModel import db, TestTable , POSTGRES # 引入資料庫相關的東西，已經打包成一個 py 檔案
+from CH_queryDB import queryDB
+from CH_insertDB import write_random_data
+
 app = Flask(__name__)
 
 print("hello")
@@ -14,11 +17,28 @@ def mingchin():
 def index():
     return render_template('index.html')
 
-
-print("hello")
-
 @app.route('/ChiaHong')
 def ChiaHong():
     return render_template('ChiaHong.html')
+
+# 設定資料庫相關的連線內容
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(password)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+# 初始化資料庫模型
+db.init_app(app)
+
+# 資料庫查詢並顯示的路由
+@app.route('/query_db')
+def query_db():
+    # 查詢 TestTable 資料表中的所有資料
+    rows = queryDB(TestTable)
+    return render_template('queryPage.html', rows=rows)
+
+@app.route('/db_insert')
+def db_insert():
+    lastIdx = queryDB(TestTable)[-1].idx
+    write_random_data('static/data.csv', db, lastIdx)
+    return 'ok'
 
 print("hello")
