c947231c1c8922e0f6995f0f81139c3457aadd78
MingChin-Kao <kmc558821@gmail.com>
2023-04-26 20:00:04 +0800

    read_csv_version

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 # main.py
  
 from flask import Flask, render_template
+import pandas as pd
 app = Flask(__name__)
 
-print("hello")
 
 @app.route('/mingchin')
 def mingchin():
     return render_template('mingchin.html')
+
+@app.route('/show_csv')
+def show_csv():
+    data = pd.read_csv('./static/data.csv', encoding='big5')
+    data_dict = data.to_dict(orient='records')
+    data_header = list(data_dict[0].keys())
+    print("data dict is ", data_dict)
+    return render_template('show_csv.html', data_header=data_header, datas=data_dict)
 
 
 @app.route('/HongEn')
@@ -15,10 +23,7 @@ def index():
     return render_template('index.html')
 
 
-print("hello")
-
 @app.route('/ChiaHong')
 def ChiaHong():
     return render_template('ChiaHong.html')
 
-print("hello")
